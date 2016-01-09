7780bd45681b455c058130f80e5482f4bdb2bf61
Khoa Nguyen <khoa02031995@gmail.com>
2016-01-09 17:33:35 +0700

    add jsx-chai

--- test/ReactHowler.spec.js
+++ test/ReactHowler.spec.js
@@ -1,6 +1,25 @@
+import React from 'react'
 import { expect } from 'chai'
-import ReactHowler from '../src/ReactHowler'
+import { createRenderer } from 'react-addons-test-utils';
+import sinon from 'sinon'
+import Audio from '../src/ReactHowler'
+
+// var spy = sinon.spy()
+//
+// Audio.__Rewire__('Howler', () => {
+//   play: spy
+// })
 
 describe('ReactHowler component', () => {
-  
+
+  it('should render as blank div tag', () => {
+    const renderer = createRenderer()
+    renderer.render(<Audio />);
+    const actualElement = renderer.getRenderOutput();
+    const expectedElement = <div />
+    console.log(actualElement)
+    console.log(expectedElement)
+
+    expect(actualElement).to.equal(expectedElement);
+  });
 })
