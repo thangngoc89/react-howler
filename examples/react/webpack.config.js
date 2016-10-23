894a47e3ee157e87b3a2f88910645661647c096f
Renan Mendes Carvalho <aitherios@gmail.com>
2016-10-23 12:56:05 -0200

    Node6 (#18)
    * Update babel packages to be compatible with node6
    
    * Add node6 to travis

--- examples/react/webpack.config.js
+++ examples/react/webpack.config.js
@@ -6,7 +6,7 @@ module.exports = {
   entry: [
     'webpack-dev-server/client?http://localhost:3000',
     'webpack/hot/only-dev-server',
-    './main.js'
+    './main'
   ],
   output: {
     filename: 'bundle.js',
