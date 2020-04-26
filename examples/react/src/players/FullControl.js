ebed0acc220192b8a4025dbd10d7edae3e063b49
Himadu2000 <49078336+Himadu2000@users.noreply.github.com>
2020-04-26 08:05:54 +0700

    Update FullControl.js
    Moving to hook based. Require edits.

--- examples/react/src/players/FullControl.js
+++ examples/react/src/players/FullControl.js
@@ -1,163 +1,126 @@
-import React from 'react'
+import React,{useState,useEffect} from 'react'
 import ReactHowler from 'ReactHowler'
 import raf from 'raf' // requestAnimationFrame polyfill
 import Button from '../components/Button'
 
-class FullControl extends React.Component {
-  constructor (props) {
-    super(props)
+const FullControl = () => {
+  const [playing, setPlaying] = useState(false)
+  const [loaded, setLoaded] = useState(false)
+  const [loop, setLoop] = useState(false)
+  const [mute, setMute] = useState(false)
+  const [volume, setVolume] = useState(1.0)
 
-    this.state = {
-      playing: false,
-      loaded: false,
-      loop: false,
-      mute: false,
-      volume: 1.0
-    }
-    this.handleToggle = this.handleToggle.bind(this)
-    this.handleOnLoad = this.handleOnLoad.bind(this)
-    this.handleOnEnd = this.handleOnEnd.bind(this)
-    this.handleOnPlay = this.handleOnPlay.bind(this)
-    this.handleStop = this.handleStop.bind(this)
-    this.renderSeekPos = this.renderSeekPos.bind(this)
-    this.handleLoopToggle = this.handleLoopToggle.bind(this)
-    this.handleMuteToggle = this.handleMuteToggle.bind(this)
+  useEffect(() => {
+    clearRAF()
+  }, [])
+
+  const handleToggle = () => {
+    setPlaying(!playing)
   }
 
-  componentWillUnmount () {
-    this.clearRAF()
+  const handleOnLoad = () => {
+    setLoaded(true)
+    //duration = player.duration()
   }
 
-  handleToggle () {
-    this.setState({
-      playing: !this.state.playing
-    })
+  const handleOnPlay = () => {
+    setPlaying(true)
+    renderSeekPos()
   }
 
-  handleOnLoad () {
-    this.setState({
-      loaded: true,
-      duration: this.player.duration()
-    })
+  const handleOnEnd = () => {
+    setPlaying(false)
+
+    clearRAF()
   }
 
-  handleOnPlay () {
-    this.setState({
-      playing: true
-    })
-    this.renderSeekPos()
+  const handleStop = () => {
+    player.stop()
+    setPlaying(false) // Need to update our local state so we don't immediately invoke autoplay
+
+    renderSeekPos()
   }
 
-  handleOnEnd () {
-    this.setState({
-      playing: false
-    })
-    this.clearRAF()
+  function handleLoopToggle() {
+    setLoop(!loop)
   }
 
-  handleStop () {
-    this.player.stop()
-    this.setState({
-      playing: false // Need to update our local state so we don't immediately invoke autoplay
-    })
-    this.renderSeekPos()
+  const handleMuteToggle = () => {
+    setMute(!mute)
   }
 
-  handleLoopToggle () {
-    this.setState({
-      loop: !this.state.loop
-    })
-  }
+  const renderSeekPos = () => {
+    // setSeek player.seek(),
 
-  handleMuteToggle () {
-    this.setState({
-      mute: !this.state.mute
-    })
-  }
-
-  renderSeekPos () {
-    this.setState({
-      seek: this.player.seek()
-    })
-    if (this.state.playing) {
-      this._raf = raf(this.renderSeekPos)
+    if (playing) {
+      _raf = raf(renderSeekPos)
     }
   }
 
-  clearRAF () {
-    raf.cancel(this._raf)
+  function clearRAF() {
+    raf.cancel(_raf)
   }
 
-  render () {
-    return (
-      <div className='full-control'>
-        <ReactHowler
-          src={['sound.ogg', 'sound.mp3']}
-          playing={this.state.playing}
-          onLoad={this.handleOnLoad}
-          onPlay={this.handleOnPlay}
-          onEnd={this.handleOnEnd}
-          loop={this.state.loop}
-          mute={this.state.mute}
-          volume={this.state.volume}
-          ref={(ref) => (this.player = ref)}
-        />
+  return (
+    <div className="full-control">
+      <ReactHowler
+        src={["sound.ogg", "sound.mp3"]}
+        playing={playing}
+        onLoad={handleOnLoad}
+        onPlay={handleOnPlay}
+        onEnd={handleOnEnd}
+        loop={loop}
+        mute={mute}
+        volume={volume}
+        ref={ref => (player = ref)}
+      />
 
-        <p>{(this.state.loaded) ? 'Loaded' : 'Loading'}</p>
+      <p>{loaded ? "Loaded" : "Loading"}</p>
 
-        <div className='toggles'>
-          <label>
-            Loop:
+      <div className="toggles">
+        <label>
+          Loop:
+          <input type="checkbox" checked={loop} onChange={handleLoopToggle} />
+        </label>
+        <label>
+          Mute:
+          <input type="checkbox" checked={mute} onChange={handleMuteToggle} />
+        </label>
+      </div>
+
+      <p>
+        {"Status: "}
+        {seek !== undefined ? seek.toFixed(2) : "0.00"}
+        {" / "}
+        {duration ? duration.toFixed(2) : "NaN"}
+      </p>
+
+      <div className="volume">
+        <label>
+          Volume:
+          <span className="slider-container">
             <input
-              type='checkbox'
-              checked={this.state.loop}
-              onChange={this.handleLoopToggle}
+              type="range"
+              min="0"
+              max="1"
+              step=".05"
+              value={volume}
+              onChange={e => setVolume(parseFloat(e.target.value))}
+              style={{ verticalAlign: "bottom" }}
             />
-          </label>
-          <label>
-            Mute:
-            <input
-              type='checkbox'
-              checked={this.state.mute}
-              onChange={this.handleMuteToggle}
-            />
-          </label>
-        </div>
-
-        <p>
-          {'Status: '}
-          {(this.state.seek !== undefined) ? this.state.seek.toFixed(2) : '0.00'}
-          {' / '}
-          {(this.state.duration) ? this.state.duration.toFixed(2) : 'NaN'}
-        </p>
-
-        <div className='volume'>
-          <label>
-            Volume:
-            <span className='slider-container'>
-              <input
-                type='range'
-                min='0'
-                max='1'
-                step='.05'
-                value={this.state.volume}
-                onChange={e => this.setState({volume: parseFloat(e.target.value)})}
-                style={{verticalAlign: 'bottom'}}
-              />
-            </span>
-            {this.state.volume.toFixed(2)}
-          </label>
-        </div>
-
-        <Button onClick={this.handleToggle}>
-          {(this.state.playing) ? 'Pause' : 'Play'}
+          </span>
+          {volume.toFixed(2)}
+        </label>
+         <Button onClick={handleToggle}>
+          {(playing) ? 'Pause' : 'Play'}
         </Button>
-        <Button onClick={this.handleStop}>
+        <Button onClick={handleStop}>
           Stop
         </Button>
       </div>
+      </div>
+
     )
   }
-}
 
 export default FullControl
